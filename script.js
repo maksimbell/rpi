9b3b9cdbb7335be5cc6bf0eb33856e99422572c6
Maksim Beliavsky <maksimbell503@gmail.com>
2022-04-12 18:07:31 +0300

    add main block with greetings

---
+++ script.js
@@ -0,0 +1,100 @@
+const body = document.body;
+const time = document.querySelector('.time');
+const dateElement = document.querySelector('.date');
+const greeting = document.querySelector('.greeting-container__greeting');
+const name = document.querySelector('.greeting-container__name');
+const calendar = document.querySelector('.calendar');
+const title = document.querySelector('.footer__title');
+const explanation = document.querySelector('.footer__explanation');
+
+let greet = 'Good';
+
+// 16.06.1995-first
+
+function showTime() {
+    const date = new Date();
+    const currentTime = date.toLocaleTimeString();
+    time.textContent = currentTime;
+    showDate();
+    showGreeting();
+    setTimeout(showTime, 1000);
+}
+
+function showDate() {
+    const date = new Date();
+    const options = {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric'
+    };
+    const currentDate = date.toLocaleDateString('en', options);
+    dateElement.textContent = currentDate;
+}
+
+function getDateCalendarString(date){
+    return `${date.getFullYear()}-${(date.getMonth()+1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+}
+
+function fillCalendarDay() {
+    const date = new Date();
+    calendar.value = `${date.getFullYear()}-${(date.getMonth()+1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
+}
+
+function getTimeOfDay() {
+    const date = new Date();
+    const hours = date.getHours();
+    if (hours >= 6 && hours < 12) {
+        return 'morning';
+    } else if (hours >= 12 && hours < 18) {
+        return 'afternoon';
+    } else if (hours >= 0 && hours < 6) {
+        return 'night';
+    } else {
+        return 'evening';
+    }
+}
+
+function showGreeting() {
+    const timeOfDay = getTimeOfDay();
+    const greetingText = `${greet} ${timeOfDay},`;
+    greeting.textContent = greetingText;
+}
+
+function getLocalStorageItem() {
+    if (localStorage.getItem('name')) {
+        name.value = localStorage.getItem('name');
+    }
+}
+
+const addLocalStorageItem = () => {
+    localStorage.setItem('name', name.value);
+}
+
+function showTitle(){
+
+}
+
+async function setBackground() {
+    const date = new Date(calendar.value);
+    const url = `https://api.nasa.gov/planetary/apod?api_key=BBa6HP19YvGFhyWhXt3ZVL8ZYWbmhdacoy6zgofP&date=${getDateCalendarString(date)}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+
+    body.style.backgroundImage = `url(${data.url})`;
+    body.style.backgroundRepeat = `no-repeat`;
+    body.style.backgroundSize = `cover`;
+    body.style.backgroundPosition = `center`;
+
+    title.textContent = `"${data.title}"`;
+    // explanation.textContent = data.explanation;
+}
+
+fillCalendarDay();
+setBackground();
+showTime();
+
+// window.addEventListener('beforeunload', addLocalStorageItem);
+window.addEventListener('load', getLocalStorageItem);
+name.addEventListener('input', addLocalStorageItem);
+calendar.addEventListener('change', setBackground);
